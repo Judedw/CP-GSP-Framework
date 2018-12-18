ad5f24c1831f23ca533e9cb4e0d692e4a85dcadf
nuwan <nuwan@ceylonit.com>
2018-12-18 13:03:33 +0530

    Load application context.

---
+++ src/main/java/com/clearpicture/platform/componant/StaticApplicationContext.java
@@ -0,0 +1,23 @@
+package com.clearpicture.platform.componant;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 
+ * @author Virajith
+ *
+ */
+public class StaticApplicationContext implements ApplicationContextAware {
+
+	private static ApplicationContext context;
+
+	public void setApplicationContext(ApplicationContext context) {
+		StaticApplicationContext.context = context;
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		return context;
+	}
+
+}
