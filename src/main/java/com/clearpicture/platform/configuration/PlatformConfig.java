8b0d518e5821f04dcd015b72c028c34ea1e86a53
nuwan <nuwan@ceylonit.com>
2019-02-12 18:19:00 +0530

    Cors filter added

--- src/main/java/com/clearpicture/platform/configuration/PlatformConfig.java
+++ src/main/java/com/clearpicture/platform/configuration/PlatformConfig.java
@@ -3,12 +3,23 @@ package com.clearpicture.platform.configuration;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.clearpicture.platform.util.PlatformConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.mail.simplemail.SimpleEmailServiceJavaMailSender;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class PlatformConfig {
@@ -31,5 +42,27 @@ public class PlatformConfig {
     @Bean
     public SimpleEmailServiceJavaMailSender simpleEmailServiceJavaMailSender(AmazonSimpleEmailService ses) {
         return new SimpleEmailServiceJavaMailSender(ses);
+    }
+
+    @Primary
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(
+            @Value("#{'${app.auth.corsAllowedOrigins}'.split(',')}") List<String> allowedOrigins) {
+
+        if (!CollectionUtils.isEmpty(allowedOrigins)) {
+            log.info("CORS allowed origins: {}",
+                    allowedOrigins.stream().collect(Collectors.joining(PlatformConstant.COMMA_DELIMITER)));
+        }
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
